a40e1a71e5cc6503c85ea64e51d645a36228f2cc
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-04-12 18:46:17 +0800

    Update 运行.bat

--- 运行.bat
+++ 运行.bat
@@ -1,7 +1,10 @@
 @echo off
 chcp 65001
 setlocal
-.venv\Scripts\python.exe main.py
+env\python.exe a_提取音频.py
+env\python.exe b_分离人声.py
+env\python.exe c_人声检测.py
+env\python.exe d_语音识别.py
 popd
 pause
 endlocal
